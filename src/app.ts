0519ee7a79010e1a419538fcc4600e6498f23bef
Rrissoli <raphels123@gmail.com>
2023-08-09 21:34:17 -0300

    config/server-express

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-
+import route from './routes';
 const app = express()
 
 
-const port = 3000
-app.listen(port)
+app.use(express.json());
+app.use(route);
+
+export {app}
